d4010e821b009286266aba553fe73c00895d1e06
s-beats <sakashin10291029@gmail.com>
2021-09-08 19:50:11 +0900

    feat: constructor

--- projects/10/Compiler/jack_analyzer.go
+++ projects/10/Compiler/jack_analyzer.go
@@ -1,3 +1,50 @@
 package main
 
-type jackAnalyzer struct{}
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type jackAnalyzer struct {
+	inputs []*os.File
+	output *os.File
+}
+
+func newJackAnalyzer(file *os.File) *jackAnalyzer {
+	jackAnalyzer := new(jackAnalyzer)
+	// ディレクトリであるか確認
+	_, err := file.ReadDir(0)
+	if err == nil {
+		// ディレクトリのケース
+		file.Seek(0, 0)
+		files, err := file.ReadDir(0)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, f := range files {
+			path := filepath.Join(file.Name(), f.Name())
+
+			// 拡張子チェック
+			sPath := strings.Split(path, ".")
+			if sPath[len(sPath)-1] != "jack" {
+				log.Printf("%s is not jack file", f.Name())
+				continue
+			}
+			input, err := os.Open(path)
+			if err != nil {
+				panic(err)
+			}
+			// FIXME: cap
+			jackAnalyzer.inputs = append(jackAnalyzer.inputs, input)
+		}
+	} else {
+		// ファイルのケース
+		jackAnalyzer.inputs = append(jackAnalyzer.inputs, file)
+	}
+	return jackAnalyzer
+}
+
+func (j *jackAnalyzer) Close() {}
