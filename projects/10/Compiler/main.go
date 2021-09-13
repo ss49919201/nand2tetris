85c988535cb1f720ccedb2b2d796b0dd44906a9e
s-beats <sakashin10291029@gmail.com>
2021-09-13 13:40:10 +0900

    feat: new analyzer

--- projects/10/Compiler/main.go
+++ projects/10/Compiler/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	log.Println("start analyze")
+	log.Printf("command line args: %s", os.Args[1:])
+
+	// Open File
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	_ = newJackAnalyzer(file)
+}
