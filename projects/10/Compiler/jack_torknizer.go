4c9c6b5ecd6925f2131fbe3571c99cb401045b49
s-beats <sakashin10291029@gmail.com>
2021-09-17 13:34:43 +0900

    nextText

--- projects/10/Compiler/jack_torknizer.go
+++ projects/10/Compiler/jack_torknizer.go
@@ -10,6 +10,7 @@ type jackTokenizer struct {
 	inputs  []*os.File
 	output  *os.File
 	token   string
+	text    string
 	scanner *bufio.Scanner
 }
 
@@ -31,7 +32,14 @@ func newJackTokenizer(inputs []*os.File, outputFileNameBase string) (*jackTokeni
 
 // 現在のファイルにテキストは存在するか
 func (j *jackTokenizer) hasMoreText() bool {
-	return false
+	return j.scanner.Scan()
+}
+
+// 次の行が存在すればtextにset
+func (j *jackTokenizer) nextText() {
+	if j.hasMoreText() {
+		j.text = j.scanner.Text()
+	}
 }
 
 // 行から1文字ずつ取り出し何らかのトークンに一致するか
